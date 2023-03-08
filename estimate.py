be2448c65289760aea9f9d5fdf5b1fc2b5d527da
fbrate <fbrate@outlook.com>
2023-03-08 16:07:22 +0100

    variable border and iterations.

--- estimate.py
+++ estimate.py
@@ -6,11 +6,13 @@
 # NOTE: WIDTH HEIGHT per PROCESSOR
 WIDTH = 100
 HEIGHT = 100
-ITERATIONS = 2520 # NEEDS TO be a LCM of all the border thicknesses
-LATENCY = 1 # in seconds?
+ITERATIONS = 12 # NEEDS TO be a LCM of all the border thicknesses
 BANDWIDTH = 3000 # in megabytes per sec?
 bytes_per_point = 8
 point_multiplier = 1 # used to multiply by for example by 3 since we always are dealing with 3 grids.
+stansils_send = 4000/3
+borders = 4
+
 
 def originalCalc():
     calculated_points = 0
@@ -29,6 +31,7 @@ def borderCalc(K):
         i += K
         j = 1
         while j < K:
+            # one extra row to calculate in top, and botom.
             extra_points += WIDTH * (K - j) * 2
             j+=1
     return (calculated_points + extra_points) * point_multiplier
@@ -54,18 +57,14 @@ if __name__ == "__main__":
     calc = []
     calc.append(ogCalc)
     print("Original points to calculate: " + str(calc[0]))
-    calc.append(borderCalc(2))
-    calc.append(borderCalc(3))
-    calc.append(borderCalc(4))
-    calc.append(borderCalc(5))
-    calc.append(borderCalc(6))
-    calc.append(borderCalc(7))
-    calc.append(borderCalc(8))
-    calc.append(borderCalc(9))
-    calc.append(borderCalc(10))
-    i = 2;
-    for x in calc:
-        print("Points for border thicknes " + str(i) + ": " + str(x) + ". Percentage inc: " + str(x/ogCalc * 100) + "%")
+    i = 2
+    while i <= borders:
+        calc.append(borderCalc(i))
+        i+=1
+    i = 0;
+    while i < borders:
+        communications = int(ITERATIONS/(i+1))
+        print("b" + str(i+1) + " " + str(calc[i]) + " " + str(calc[i]/ogCalc * 100) + "%" + " " + str(communications))
         i+=1
 
     originalCom(calc)
